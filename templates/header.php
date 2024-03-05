ef545a009d99b7465595bade384a66f4fd77ce95
gm485 <b00156485@mytudublin.ie>
2024-03-05 18:23:43 +0000

    crud part 1

---
+++ templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Simple Database</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <a href="/public/index.html">hi</a>
+</head>
+<body>
+<h1>Simple Database</h1>
